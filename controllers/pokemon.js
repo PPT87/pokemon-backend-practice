2792b69fd1581595cddb26209c8114fad4594da6
PeterT <tranpp87@gmail.com>
2022-03-09 23:38:21 -0500

    change update res.status to 200 to resolve edit functionality in console

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -50,7 +50,7 @@ const deletePokemon = async (req, res) => {
 const update = async (req, res) => {
   try{
     const pokemon = await Pokemon.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    res.status(204).json(pokemon)
+    res.status(200).json(pokemon)
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
